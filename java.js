3fb4c587dcf352125b8d4031d2706c3ab0ca86d1
Troy Smith <troy.smith@mymona.uwi.edu>
2018-11-12 03:31:26 +0000

    ex4

--- java.js
+++ java.js
@@ -2,8 +2,9 @@ window.onload = function(){
    
         let search_value = document.getElementById("search");
         let result = document.getElementById("result");
-        let button =  document.getElementById("buta");
-        button.addEventListener("click", function(){
+             let buttonA =  document.getElementById("buta");
+        let buttonB =  document.getElementById("butb");
+        buttonA.addEventListener("click", function(){
        let xml = new XMLHttpRequest();
        xml.onreadystatechange = function(){
            if(xml.readyState === 4 && xml.status === 200){
@@ -14,4 +15,22 @@ window.onload = function(){
          xml.open("GET","request.php?q="+search_value.value,true);
        xml.send();
     });
+    buttonB.addEventListener("click", function(){
+       let xml = new XMLHttpRequest();
+       xml.onreadystatechange = function(){
+           if(xml.readyState === 4 && xml.status === 200){
+               let output = "<ol>";
+               let text = xml.responseXML;
+               let terms = text.getElementsByTagName("definition");
+               for(let counter = 0; counter<terms.length;counter++){
+                   output+="<li><h3>"+terms[counter].getAttribute("name")+"</h3><p>"+terms[counter].childNodes[0].nodeValue+"</p><p> -"+terms[counter].getAttribute("author")+"</p></li>";
+               }
+               output+="</ol>";
+               result.innerHTML ="<h2>Result</h2>"+output;
+           }
+       };
+       xml.open("GET","request.php?q=&all=true",true);
+       xml.send();
+    });
+
 }; 
--- request.php
+++ request.php
@@ -14,5 +14,32 @@ $definition = [
     "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
 ];
 
-print "<h3>" . strtoupper($query) . "</h3>";
-print "<p>" . $definition[$query] . "</p>";
+//print "<h3>" . strtoupper($query) . "</h3>";
+//print "<p>" . $definition[$query] . "</p>";
+ $xmldata = '<?xml version="1.0" encoding="UTF-8"?>
+<entries>
+    <definition name ="definition" author = "SpongeBob">
+        A statement of the exact meaning of a word, especially in a dictionary.
+    </definition>
+    <definition name ="bar" author = "Krabbs">
+        A place that sells alcholic beverages.
+    </definition>
+    <definition name ="ajax" author = "Sandy">
+        Technique which involves the use of javascript and xml (or JSON).
+    </definition>
+    <definition name ="html" author = "Pearl">
+       The standard markup language for creating web pages and web applications.
+    </definition>
+    <definition name ="css" author = "Patrick">
+        A style sheet language used for describing the presentation of a document written in a markup language.
+    </definition>
+     <definition name ="javascript" author = "Squidpird">
+        A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.
+    </definition>
+     <definition name ="php" author = "Dutchman">
+        A server-side scripting language, and a powerful tool for making dynamic and interactive websites.
+    </definition>
+</entries>';
+header('Content-Type: text/xml');
+$xmlOutput = new SimpleXMLElement($xmldata);
+echo $xmlOutput->asXML(); 
