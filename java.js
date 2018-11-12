ab1657029358216dbf377a6e63bac896fea8ad1f
Troy Smith <troy.smith@mymona.uwi.edu>
2018-11-12 03:21:19 +0000

    ex3

--- java.js
+++ java.js
@@ -1,12 +1,17 @@
 window.onload = function(){
-    document.getElementById("buta").addEventListener("click", function(){
-       let xml = new  XMLHttpRequest();
+   
+        let search_value = document.getElementById("search");
+        let result = document.getElementById("result");
+        let button =  document.getElementById("buta");
+        button.addEventListener("click", function(){
+       let xml = new XMLHttpRequest();
        xml.onreadystatechange = function(){
            if(xml.readyState === 4 && xml.status === 200){
-               alert(xml.responseText);
+               result.innerHTML ="<h3>Result</h3>"+xml.responseText;
            }
        };
-       xml.open("GET","request.php?q=definition",true);
+     
+         xml.open("GET","request.php?q="+search_value.value,true);
        xml.send();
     });
 }; 
